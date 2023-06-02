d6fba79016a2252c9b7e5dbc2264ffc8c2516154
Thoriq R <respect.thoriq@gmail.com>
2023-06-02 10:37:48 +0700

    send message design

--- resources/views/livewire/chat/send-message.blade.php
+++ resources/views/livewire/chat/send-message.blade.php
@@ -2,5 +2,9 @@
     {{-- Nothing in the world is as soft and yielding as water. --}}
     <div class="chatbox_footer">
         
+        <div class="custom_form_group">
+            <input type="text" class="control" placeholder="write message">
+            <button class="submit" type="submit">enter</button>
+        </div>
     </div>
 </div>
